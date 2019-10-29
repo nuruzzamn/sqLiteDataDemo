65710197941d72a999877de8e3a23b176aabff5f
Your Name <nur.mnzaman@gmail.com>
2019-10-30 04:11:21 +0600

    check

---
+++ app/src/main/java/sqlite/example/com/sqlitedata/DashboardActivity.java
@@ -0,0 +1,56 @@
+package sqlite.example.com.sqlitedata;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.jar.Attributes;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    String EmailHolder,NameHolder;
+    TextView Email,Name;
+    Button LogOUT ;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+        Email = (TextView)findViewById(R.id.textView1);
+        LogOUT = (Button)findViewById(R.id.button1);
+
+        Intent intent = getIntent();
+
+        // Receiving User Email Send By MainActivity.
+       EmailHolder = intent.getStringExtra(MainActivity.UserEmail);
+
+
+        // Setting up received email to TextView.
+      // Email.setText(Email.getText().toString()+ EmailHolder);
+
+
+        // Adding click listener to Log Out button.
+        LogOUT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Finishing current DashBoard activity on button click.
+                finish();
+
+                Toast.makeText(DashboardActivity.this,"Log Out Successfull", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
+
+}
